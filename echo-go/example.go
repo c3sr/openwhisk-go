16defb894b00be974258ea26d6cd17e0e6474e9e
Carl Pearson <pearson@illinois.edu>
2017-02-07 17:59:07 -0600

    Make last line of output stringified json, remove example.c

--- echo-go/example.go
+++ echo-go/example.go
@@ -1,1 +1,12 @@
-package c3sr
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("This is an example log message from a Go program.")
+	fmt.Printf("{ \"msg\": \"Hello from arbitrary Go program!\", \"args\": %s }\n", os.Args[1:])
+
+}
