c35fa04452a7543013b52e17125ca880d2167f52
Carl Pearson <pearson@illinois.edu>
2017-02-21 19:51:10 -0600

    Handwritten client/model for invocation

---
+++ models/action.go
@@ -0,0 +1,18 @@
+package models
+
+type Action struct {
+	Name        string
+	Publish     bool
+	Annotations Annotations
+	version     string
+	Namespace   string
+}
+
+type Annotation struct {
+	Key   string
+	Value interface{}
+}
+
+type Annotations []Annotation
+
+type Actions []Action
